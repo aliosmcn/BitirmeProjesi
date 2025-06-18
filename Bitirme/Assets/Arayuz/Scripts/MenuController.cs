e29a6cafaa781b666b351270f437afa8ec8dd783
Ali Osman <67677618+aliosmcn@users.noreply.github.com>
2025-06-19 02:08:10 +0300

    main menu

--- Bitirme/Assets/Arayuz/Scripts/MenuController.cs
+++ Bitirme/Assets/Arayuz/Scripts/MenuController.cs
@@ -1,10 +1,22 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
     public GameObject creditsPanel;
-    
+
+    private void Start()
+    {
+        creditsPanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!creditsPanel) return;
+        if (Input.GetKeyDown(KeyCode.Escape)) creditsPanel.SetActive(false);
+    }
+
     public void StartButton()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
